8b315c6a552d965070180a2695c2fb2ae881d96c
Daniel Turcios <danielturciosa@gmail.com>
2022-09-13 16:26:58 -0700

    conditionals

---
+++ Intro to JS/cond_app.js
@@ -0,0 +1,10 @@
+let random = Math.random();
+if (random < 0.5) {
+    console.log("YOUR NUMBEWR IS LESS THAN 0.5!!");
+    console.log(random);
+}
+
+if (random >= 0.5) {
+    console.log("YOUR NUMBER IS GRATER THAN 0.5!!!");
+    console.log(random);
+}
