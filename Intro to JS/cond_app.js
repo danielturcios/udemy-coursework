52f0afa80db11f8e30b825c7c595a20ba98eae13
Daniel Turcios <danielturciosa@gmail.com>
2022-09-13 20:05:06 -0700

    section 16 part 168 - nested conditionals

--- Intro to JS/cond_app.js
+++ Intro to JS/cond_app.js
@@ -1,22 +1,20 @@
-// let random = Math.random();
-// if (random < 0.5) {
-//     console.log("YOUR NUMBEWR IS LESS THAN 0.5!!");
-//     console.log(random);
-// }
+let random = Math.random();
+if (random < 0.5) {
+    console.log("YOUR NUMBEWR IS LESS THAN 0.5!!");
+    console.log(random);
+} else {
+    console.log("YOUR NUMBER IS GREATER (OR EQUAL) THAN 0.5!!!");
+}
+// const dayOfWeek = prompt("ENTER A DAY").toLowerCase();
 
-// if (random >= 0.5) {
-//     console.log("YOUR NUMBER IS GRATER THAN 0.5!!!");
-//     console.log(random);
-// }
-
-// const dayOfWeek = 'Friday';
-
-// if (dayOfWeek === 'Monday') {
+// if (dayOfWeek === 'monday') {
 //     console.log("UGHHH I HATE MONDAYS!");
-// } else if (dayOfWeek === 'Saturday') {
+// } else if (dayOfWeek === 'saturday') {
 //     console.log("YAY I LOVE SATURDAYS!");
-// } else if (dayOfWeek === 'Friday') {
+// } else if (dayOfWeek === 'friday') {
 //     console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+// } else {
+//     console.log("MEH");
 // }
 
 // 0-5 - free
@@ -24,12 +22,28 @@
 // 10-65 Adult $20
 // 65+ senior $10
 
-const age = 8;
+// const age = 890;
 
-if (age < 5) {
-    console.log("You are a baby. You get in for free!");
-} else if (age < 10) {
-    console.log("You are a child. You pay $10");
-} else if (age < 65) {
-    console.log("You are an adult. You pay $20");
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free!");
+// } else if (age < 10) {
+//     console.log("You are a child. You pay $10");
+// } else if (age < 65) {
+//     console.log("You are an adult. You pay $20");
+// } else {
+//     console.log("You are a senior. You pay $10");
+// }
+
+const password = prompt("please enter a new password");
+
+// Password must be 6+ characters
+if (password.length >= 6) {
+    // Pass word cannot include space
+    if (password.indexOf(' ') === -1) {
+        console.log("Valid Password!");
+    } else {
+        console.log("Password cannot contain any spaces")
+    }
+} else {
+    console.log("PASSWORD TO SHORT! Must be 6+ characters");
 }
