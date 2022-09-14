ee3ea8b54b5f74f7803479152aeea946924c9e66
Daniel Turcios <danielturciosa@gmail.com>
2022-09-13 17:22:49 -0700

    section 16 part 166 - else if

--- Intro to JS/cond_app.js
+++ Intro to JS/cond_app.js
@@ -1,10 +1,35 @@
-let random = Math.random();
-if (random < 0.5) {
-    console.log("YOUR NUMBEWR IS LESS THAN 0.5!!");
-    console.log(random);
-}
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("YOUR NUMBEWR IS LESS THAN 0.5!!");
+//     console.log(random);
+// }
 
-if (random >= 0.5) {
-    console.log("YOUR NUMBER IS GRATER THAN 0.5!!!");
-    console.log(random);
+// if (random >= 0.5) {
+//     console.log("YOUR NUMBER IS GRATER THAN 0.5!!!");
+//     console.log(random);
+// }
+
+// const dayOfWeek = 'Friday';
+
+// if (dayOfWeek === 'Monday') {
+//     console.log("UGHHH I HATE MONDAYS!");
+// } else if (dayOfWeek === 'Saturday') {
+//     console.log("YAY I LOVE SATURDAYS!");
+// } else if (dayOfWeek === 'Friday') {
+//     console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+// }
+
+// 0-5 - free
+// 5-10 Child $10
+// 10-65 Adult $20
+// 65+ senior $10
+
+const age = 8;
+
+if (age < 5) {
+    console.log("You are a baby. You get in for free!");
+} else if (age < 10) {
+    console.log("You are a child. You pay $10");
+} else if (age < 65) {
+    console.log("You are an adult. You pay $20");
 }
