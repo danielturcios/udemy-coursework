5ea71c367a470ded8523ab92933e18eb868ecf2c
Daniel Turcios <danielturciosa@gmail.com>
2022-09-19 18:39:45 -0700

    sectoin 16 part 171

--- Intro to JS/logic_app.js
+++ Intro to JS/logic_app.js
@@ -14,8 +14,10 @@
 const age = 90;
 if (age >= 0 && age < 5 || age >= 65) {
     console.log("FREE");
-} else if (age < 10) {
+} else if (age >= 5 && age < 10) {
     console.log("$10");
-} else if (age < 65) {
-    console.log("$20")
+} else if (age >= 10 && age < 65) {
+    console.log("$20");
+} else {
+    console.log("INVALID AGE");
 }
