cccad44833522b064d77cb046c10474a1e0b8159
Daniel Turcios <danielturciosa@gmail.com>
2022-10-04 17:37:04 -0700

    sectoin 19 part 192 - more for loops

--- Intro to JS/Basics/loops_app.js
+++ Intro to JS/Basics/loops_app.js
@@ -1,3 +1,15 @@
-for (let num = 1; num <= 10; num++) {
-    console.log(num);
+// for (let num = 1; num <= 10; num++) {
+//     console.log(num);
+// }
+
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i);
+// }
+
+for (let i = 10; i <= 1000; i *= 10) {
+    console.log(i);
 }
