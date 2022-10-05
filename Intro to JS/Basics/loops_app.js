25371af0fc2266161cf265aed188036688f1dba8
Daniel Turcios <danielturciosa@gmail.com>
2022-10-04 17:49:04 -0700

    section 19 part 193 - inifinite for loops

--- Intro to JS/Basics/loops_app.js
+++ Intro to JS/Basics/loops_app.js
@@ -10,6 +10,18 @@
 //     console.log(i);
 // }
 
-for (let i = 10; i <= 1000; i *= 10) {
-    console.log(i);
-}
+// for (let i = 10; i <= 1000; i *= 10) {
+//     console.log(i);
+// }
+
+// DO NOT RUN THIS LOOP
+// INFINITE LOOP!! BAD!!
+// =====================
+// =====================
+// for (let i=20; i>=0; i++) {
+//     console.log(i);
+// }
+// =====================
+// =====================
+// DO NOT RUN THIS LOOP
+// INFINITE LOOP!! BAD!!
